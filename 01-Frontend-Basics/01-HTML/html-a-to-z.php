3143c7d1b7dee2c96a7204bbcf532845d9fbf90f
Lingkon <lingkon707@gmail.com>
2025-09-08 20:58:12 +0600

    Syntax - HTML CSS

---
+++ 01-Frontend-Basics/01-HTML/html-a-to-z.php
@@ -0,0 +1,121 @@
+A – Anchor <a>
+<a href="https://www.google.com" target="_blank">Go to Google</a>
+
+B – Bold <b>
+<b>This is bold text</b>
+
+C – Comment <!-- -->
+<!-- This is a comment -->
+
+D – Div <div>
+<div style="background-color: lightblue; padding: 20px;">This is a div</div>
+
+E – Emphasis <em>
+<em>This text is emphasized</em>
+
+F – Form <form>
+<form action="#" method="post">
+  Name: <input type="text" name="name"><br>
+  Email: <input type="email" name="email"><br>
+  <input type="submit" value="Submit">
+</form>
+
+G – Image <img>
+<img src="https://via.placeholder.com/150" alt="Placeholder Image">
+
+H – Headings <h1> to <h6>
+<h1>Heading 1</h1>
+<h2>Heading 2</h2>
+<h3>Heading 3</h3>
+
+I – Iframe <iframe>
+<iframe src="https://www.example.com" width="300" height="200"></iframe>
+
+J – JavaScript <script>
+<script>
+  alert('Hello World!');
+</script>
+
+K – Keyboard <kbd>
+Press <kbd>Ctrl + C</kbd> to copy
+
+L – List <ul> / <ol> / <li>
+<ul>
+  <li>Apple</li>
+  <li>Banana</li>
+</ul>
+<ol>
+  <li>First</li>
+  <li>Second</li>
+</ol>
+
+M – Marquee <marquee> (legacy)
+<marquee behavior="scroll" direction="left">Scrolling Text</marquee>
+
+N – Navigation <nav>
+<nav>
+  <a href="#">Home</a> |
+  <a href="#">About</a>
+</nav>
+
+O – Ordered List <ol>
+<ol>
+  <li>HTML</li>
+  <li>CSS</li>
+</ol>
+
+P – Paragraph <p>
+<p>This is a paragraph.</p>
+
+Q – Quote <blockquote>
+<blockquote cite="https://www.example.com">
+  “This is a blockquote example.”
+</blockquote>
+
+R – Radio Button <input type="radio">
+<form>
+  <input type="radio" name="color" value="red"> Red
+  <input type="radio" name="color" value="blue"> Blue
+</form>
+
+S – Span / Section <span> / <section>
+<section style="background-color: lightblue; padding: 10px;">
+  Section content <span style="color:red;">Span text</span>
+</section>
+
+T – Table <table>
+<table border="1">
+  <tr><th>Name</th><th>Age</th></tr>
+  <tr><td>Ali</td><td>25</td></tr>
+</table>
+
+U – Underline <u>
+<p>This is <u>underlined</u> text.</p>
+
+V – Video <video>
+<video width="320" height="240" controls>
+  <source src="https://www.w3schools.com/html/mov_bbb.mp4" type="video/mp4">
+</video>
+
+W – Wrapper <div> example
+<div style="width:500px; margin:0 auto; padding:10px; background-color:lightgreen;">
+  Wrapper div
+</div>
+
+X – XHTML / XML example
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+  <body>
+    <p>XHTML content</p>
+    <img src="https://via.placeholder.com/150" alt="Image"/>
+  </body>
+</html>
+
+Y – YouTube Embed <iframe>
+<iframe width="560" height="315" 
+  src="https://www.youtube.com/embed/dQw4w9WgXcQ" 
+  frameborder="0" allowfullscreen></iframe>
+
+Z – Z-index layering
+<div style="width:150px;height:150px;background-color:coral;position:absolute;top:50px;left:50px;z-index:1;">Box 1</div>
+<div style="width:150px;height:150px;background-color:lightblue;position:absolute;top:100px;left:100px;z-index:2;">Box 2</div>
